65e65177bac66534eafcb49c03b316140d537c51
OnePointFive99 <lzystan2021@163.com>
2023-11-08 12:57:14 +0800

    更新 两两交换链表节点

--- 两两交换链表中的节点.cpp
+++ 两两交换链表中的节点.cpp
@@ -1,3 +1,5 @@
+
+v1.0:通过这道题才算是真正理解了cur与cur->next的区别！
 /**
  * Definition for singly-linked list.
  * struct ListNode {
@@ -13,16 +15,15 @@ public:
     ListNode* swapPairs(ListNode* head) {
         ListNode *dummyHead = new ListNode(0);
         dummyHead->next = head;
-        ListNode *cur = dummyHead; 
-
+        ListNode *cur = dummyHead;
         while(cur->next&&cur->next->next){
-            ListNode* tmp = cur->next; // 记录临时节点
-            ListNode* tmp1 = cur->next->next->next; // 记录临时节点
-            cur->next = cur->next->next;    // 步骤一
-            cur->next->next = tmp;          // 步骤二
-            cur->next->next->next = tmp1;   // 步骤三
-            cur = cur->next->next; // cur移动两位，准备下一轮交换
+            ListNode *tmp1 = cur->next->next->next;//3号
+            ListNode *tmp2 = cur->next->next;//2号
+            cur->next->next->next =  cur->next;//2号指向1号
+            cur->next->next = tmp1;//1号指向3号
+            cur->next = tmp2;//cur指向2号
+            cur= cur->next->next;//cur移动到交换后的1号 
         }
-        return  dummyHead->next;
+        return dummyHead->next;
     }
 };
