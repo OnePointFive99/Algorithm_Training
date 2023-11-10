68823f4482f56bf1b156241953fca1d233b8b1e6
OnePointFive99 <lzystan2021@163.com>
2023-11-10 23:24:01 +0800

    更新 有效字母的异位词

--- 有效的字母异位词.cpp
+++ 有效的字母异位词.cpp
@@ -18,3 +18,46 @@ public:
 
     }
 };
+
+v2.0：
+嘛  既然知道是97~122了，可以缩减下数组大小
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if(s.size()!=t.size()) return false;
+        int a[26] = {0};
+        const int m = 97;
+        for(auto c: s){
+            a[static_cast<int>(c)-m]++;
+        }
+        for(auto c: t){
+            a[static_cast<int>(c)-m]--;
+        }
+        for(int i:a){
+            if(i!=0) return false;
+        }
+        return true;
+
+    }
+};
+
+v3.0：只需要-'a'就行了...不用记ascii码
+
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if(s.size()!=t.size()) return false;
+        int a[26] = {0};
+        for(auto c: s){
+            a[c-'a']++;
+        }
+        for(auto c: t){
+            a[c-'a']--;
+        }
+        for(int i:a){
+            if(i!=0) return false;
+        }
+        return true;
+
+    }
+};
