364fc2582908fc4b77fda1e1147cce5f8dfccba2
OnePointFive99 <lzystan2021@163.com>
2023-11-18 16:51:59 +0800

    更新  前序遍历、中序遍历

--- 二叉树中序遍历.cpp
+++ 二叉树中序遍历.cpp
@@ -1,3 +1,5 @@
+
+v1.0:注意理解递归的思想，递归就是套娃，递归到叶子节点就会返回
 /**
  * Definition for a binary tree node.
  * struct TreeNode {
@@ -14,11 +16,12 @@ public:
     void traversal(vector<int> &ret, TreeNode *cur)
     {
         if(cur==nullptr)return;
-        ret.push_back(cur->val);
         traversal(ret, cur->left);
+        ret.push_back(cur->val);
         traversal(ret, cur->right);
     }
-    vector<int> preorderTraversal(TreeNode* root) {
+
+    vector<int> inorderTraversal(TreeNode* root) {
         vector<int> result;
         traversal(result, root);
         return result;
