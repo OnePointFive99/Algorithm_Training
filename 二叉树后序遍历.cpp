e344c84b618255c762487f7ea64f93dd1e59ffcd
OnePointFive99 <lzystan2021@163.com>
2023-11-18 16:55:25 +0800

    添加  后序遍历

---
+++ 二叉树后序遍历.cpp
@@ -0,0 +1,27 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    void traversal(vector<int> &ret, TreeNode *cur)
+    {
+        if(cur==nullptr)return;
+        traversal(ret, cur->left);
+        traversal(ret, cur->right);
+        ret.push_back(cur->val);
+    }
+
+    vector<int> postorderTraversal(TreeNode* root) {
+        vector<int> result;
+        traversal(result, root);
+        return result;
+    }
+};
