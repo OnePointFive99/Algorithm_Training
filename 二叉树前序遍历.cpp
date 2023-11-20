5142750128e89b8f479a9d0b774f439adc0188f1
OnePointFive99 <lzystan2021@163.com>
2023-11-20 17:00:01 +0800

    添加 迭代法

--- 二叉树前序遍历.cpp
+++ 二叉树前序遍历.cpp
@@ -1,3 +1,5 @@
+
+v1.0:递归法：
 /**
  * Definition for a binary tree node.
  * struct TreeNode {
@@ -21,6 +23,37 @@ public:
     vector<int> preorderTraversal(TreeNode* root) {
         vector<int> result;
         traversal(result, root);
+        return result;
+    }
+};
+
+v2.0:迭代法：建立一个装树的节点的栈，然后更新栈时把夫节点取出，放入子节点即可
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> preorderTraversal(TreeNode* root) {
+        vector<int> result;
+        if (root == NULL) return result;
+        stack<TreeNode*> stk;
+        stk.push(root);
+        while(!stk.empty())
+        {
+            TreeNode * cur = stk.top();
+            stk.pop();
+            result.push_back(cur->val);
+            if(cur->right)stk.push(cur->right);
+            if(cur->left)stk.push(cur->left);
+        }
         return result;
     }
 };
